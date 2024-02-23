2d7fae3fe35f1ab3bfe297f67dd56ebf55858643
ShubhamGupta9453 <shubhamgupta9454666551@gmail.com>
2024-02-23 21:12:04 +0530

    Modified firebase file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -12,24 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const data = {
-  postedOn: "2023-10-24",
-  title: "Frontend Developer",
-  type: "Full time",
-  location: "In-office",
-  experience: "Fresher",
-  company: "Amazon",
-  skills: ["Javascript", "Nextjs", "Tailwind CSS"],
-  job_link: "https://www.amazon.jobs/en/",
-};
-const addJob = async () => {
-  try {
-    const docRef = await addDoc(collection(db, "jobs"), data);
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
-addJob();
 
 export { db };
